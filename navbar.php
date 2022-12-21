efdc403a027910dea61ed6363dbcb0e2d85b1303
Rajan21252125 <99201190+Rajan21252125@users.noreply.github.com>
2022-12-21 22:30:04 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #232629; padding-left:20px; font-size:31.jpg0px;"><img src="img/1-removebg-preview.png" style="height : 55px; width : 95px"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #011647;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #011647;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #011647;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #011647;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
